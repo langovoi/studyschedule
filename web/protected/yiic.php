e08b7e2b6e0fff5b3c29ace7bd92cf1b127779bd
Mark Langovoi <marklangovoi@gmail.com>
2014-09-26 18:18:31 +0400

    Fix yiic

--- web/protected/yiic.php
+++ web/protected/yiic.php
@@ -2,6 +2,12 @@
 
 // change the following paths if necessary
 $yiic=dirname(__FILE__).'/../../vendor/yiisoft/yii/framework/yiic.php';
+
+if (file_exists('../.local')) {
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
+}
+
 $config=dirname(__FILE__).'/config/console.php';
 
 require_once($yiic);
