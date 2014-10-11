ef49f20dc51d87ce707cbe0486c69962683206d3
Mark Langovoi <marklangovoi@gmail.com>
2014-10-11 21:09:01 +0400

    Исправления ошибок

--- controllers/GroupController.php
+++ controllers/GroupController.php
@@ -21,7 +21,7 @@ class GroupController extends Controller
             /** @var Group $group */
             if (!$group)
                 throw new CHttpException(404, 'Данной группы не существует');
-            if ($group->owner_id != Yii::app()->user->getId())
+            if ($group->owner_id != Yii::app()->user->getId() || !Yii::app()->user->checkAccess('admin'))
                 throw new CHttpException(403, 'У вас нет доступа к данной группе');
             self::$group = $group;
         } else
