fddb54d95a490e1f26f7ea6851597fa7a3e123e0
Mark Langovoi <marklangovoi@gmail.com>
2014-10-11 22:17:52 +0400

    Добавлено редактирование элементов

--- controllers/GroupController.php
+++ controllers/GroupController.php
@@ -89,17 +89,49 @@ class GroupController extends Controller
                 if (($key = array_search($element->number, $numbers)) !== false)
                     unset($numbers[$key]);
             }
+        $this->render('schedule/form', ['model' => $model, 'classrooms' => $classrooms, 'teachers' => $teachers, 'subjects' => $subjects, 'numbers' => $numbers]);
+    }
 
-        /*$schedule = [];
-        $schedule_model = new ScheduleElement();
-        for($i = 1; $i <= 2; $i++) {
-            for($j = 1; $j <= 6; $j++) {
-                $criteria = new CDbCriteria();
-                $criteria->addColumnCondition(['group_id' => self::$group->id, 'week_number' => $i, 'week_day' => $j]);
-                $criteria->order = 'number';
-                $schedule[$i][$j] = $schedule_model->with(['teacher', 'subject', 'classroom'])->findAll($criteria);
+    public function actionUpdateScheduleElement($element_id)
+    {
+        $model = ScheduleElement::model()->findByPk($element_id);
+        if(!$model)
+            throw new CHttpException(404, 'Элемент не найден');
+        $semester = Semesters::model()->byStartDate()->find();
+        if (Yii::app()->request->isPostRequest) {
+            $schedule_element = Yii::app()->request->getParam('ScheduleElement');
+            $week_number = $model->week_number;
+            $week_day = $model->week_day;
+            $model->setAttributes($schedule_element);
+            $model->setAttributes([
+                'week_number' => $week_number,
+                'week_day' => $week_day,
+                'group_id' => self::$group->id,
+                'semester_id' => $semester->id
+            ]);
+            if ($model->save()) {
+                Yii::app()->user->setFlash('success', 'Элемент успешно создан');
+                $this->redirect(['schedule', 'id' => self::$group->number]);
             }
-        }*/
+        }
+        $classrooms = ['' => '-'];
+        $subjects = ['' => '-'];
+        $teachers = ['' => '-'];
+        foreach (Classrooms::model()->findAll() as $classroom) {
+            $classrooms[$classroom->id] = $classroom->name;
+        }
+        foreach (Subjects::model()->findAll() as $subject) {
+            $subjects[$subject->id] = $subject->name;
+        }
+        foreach (Teachers::model()->findAll() as $teacher) {
+            $teachers[$teacher->id] = join(' ', [$teacher->lastname, $teacher->firstname, $teacher->middlename]);
+        }
+        $numbers = [1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5];
+        if (($elements = ScheduleElement::model()->findAllByAttributes(['group_id' => self::$group->id, 'week_number' => $model->week_number, 'week_day' => $model->week_day, 'semester_id' => $semester->id])))
+            foreach ($elements as $element) {
+                if (($key = array_search($element->number, $numbers)) !== false)
+                    unset($numbers[$key]);
+            }
         $this->render('schedule/form', ['model' => $model, 'classrooms' => $classrooms, 'teachers' => $teachers, 'subjects' => $subjects, 'numbers' => $numbers]);
     }
 }
