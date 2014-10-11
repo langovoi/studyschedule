96f3893789945edf1317c7366b6a97074a5a1a1d
Mark Langovoi <marklangovoi@gmail.com>
2014-10-11 18:59:26 +0400

    Исправлена сортировка и отображение

--- controllers/GroupController.php
+++ controllers/GroupController.php
@@ -42,7 +42,10 @@ class GroupController extends Controller
         $schedule_model = new ScheduleElement();
         for($i = 1; $i <= 2; $i++) {
             for($j = 1; $j <= 6; $j++) {
-                $schedule[$i][$j] = $schedule_model->with(['teacher', 'subject', 'classroom'])->findAllByAttributes(['group_id' => self::$group->id, 'week_number' => $i, 'week_day' => $j]);
+                $criteria = new CDbCriteria();
+                $criteria->addColumnCondition(['group_id' => self::$group->id, 'week_number' => $i, 'week_day' => $j]);
+                $criteria->order = 'number';
+                $schedule[$i][$j] = $schedule_model->with(['teacher', 'subject', 'classroom'])->findAll($criteria);
             }
         }
         $this->render('schedule', ['group' => self::$group, 'schedule' => $schedule]);
