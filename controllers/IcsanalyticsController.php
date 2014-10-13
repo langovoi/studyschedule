67d9484a1374f1368d0bf719ec820c2928e13d23
Mark Langovoi <marklangovoi@gmail.com>
2014-10-13 22:06:28 +0400

    Мелкие исправления

--- controllers/IcsanalyticsController.php
+++ controllers/IcsanalyticsController.php
@@ -25,7 +25,7 @@ class IcsAnalyticsController extends Controller
         $count = $logs->count();
         if (($page - 1) * 10 < $count || $page == 1) {
             $logs = $logs->findAll(['order' => 'time DESC, id DESC', 'limit' => 10, 'offset' => ($page - 1) * 10]);
-            $this->render('list', ['logs' => $logs, 'model' => $model, 'pages' => floor($count / 10), 'page' => $page]);
+            $this->render('list', ['logs' => $logs, 'model' => $model, 'pages' => ceil($count / 10), 'page' => $page]);
         } else
             throw new CHttpException(404, 'Такой страницы нет :(');
     }
