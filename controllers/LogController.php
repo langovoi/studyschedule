2a724b214834a1b0b5879f9c5da87b1e2560403b
Mark Langovoi <marklangovoi@gmail.com>
2014-10-13 22:05:56 +0400

    Мелкие исправления

--- controllers/LogController.php
+++ controllers/LogController.php
@@ -25,7 +25,7 @@ class LogController extends Controller
         $count = $logs->count();
         if (($page - 1) * 10 < $count || $page == 1) {
             $logs = $logs->findAll(['order' => 'creationdate DESC, id DESC', 'limit' => 10, 'offset' => ($page - 1) * 10]);
-            $this->render('list', ['logs' => $logs, 'model' => $model, 'pages' => round($count / 10, 0, PHP_ROUND_HALF_DOWN), 'page' => $page]);
+            $this->render('list', ['logs' => $logs, 'model' => $model, 'pages' => floor($count / 10), 'page' => $page]);
         } else
             throw new CHttpException(404, 'Такой страницы нет :(');
     }
