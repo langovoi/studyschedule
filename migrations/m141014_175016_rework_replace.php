1be1c4cf179e79e097af3b335d2b0e194275983d
Mark Langovoi <marklangovoi@gmail.com>
2014-10-14 22:07:13 +0400

    Поправил миграцию

--- migrations/m141014_175016_rework_replace.php
+++ migrations/m141014_175016_rework_replace.php
@@ -7,7 +7,7 @@ class m141014_175016_rework_replace extends CDbMigration
         $this->addColumn('{{group_replace}}', 'owner', 'string NOT NULL');
         $replaces = GroupReplace::model()->findAll();
         foreach ($replaces as $replace) {
-            $log = ActiveRecordLog::model()->findByAttributes(['model' => 'GroupReplace', 'idModel' => $replace->id]);
+            $log = ActiveRecordLog::model()->findByAttributes(['model' => 'GroupReplace', 'idModel' => $replace->id, 'action' => 'CREATE']);
             if ($log) {
                 $log = explode(' ', $log->description);
                 $replace->owner = $log[1];
