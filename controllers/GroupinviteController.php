5b3b6ba708db36dbc69f9d30525b73ef72487215
Mark Langovoi <marklangovoi@gmail.com>
2014-10-20 19:50:29 +0400

    Добавлена отклонение принятых заявок

--- controllers/GroupinviteController.php
+++ controllers/GroupinviteController.php
@@ -60,22 +60,25 @@ class GroupInviteController extends Controller
 
     public function actionDecline($id)
     {
-        if (!($model = Invite::model()->findByPk($id)) || $model->status != Invite::INVITE_CREATE)
+        if (!($model = Invite::model()->findByPk($id)) || !in_array($model->status, [Invite::INVITE_CREATE, Invite::INVITE_ACCEPT]))
             throw new CHttpException(404);
-        $mail = new YiiMailer();
-        $mail->setView('invite/decline');
-        $mail->setFrom('marklangovoi@gmail.com', 'Система управления учебным расписанием');
-        $mail->setTo($model->email);
-        $mail->setSubject('Заявка на создание');
-        $mail->send();
-        if ($mail->send()) {
-            $model->status = Invite::INVITE_DECLINE;
-            if ($model->save())
-                Yii::app()->user->setFlash('success', 'Заявка успешно отклонена');
-            else
-                Yii::app()->user->setFlash('error', 'Ошибка при смене статуса');
-        } else
-            Yii::app()->user->setFlash('error', 'Ошибка при отправке письма');
+        $model->status = Invite::INVITE_DECLINE;
+        if ($model->save())
+            Yii::app()->user->setFlash('success', 'Заявка успешно отклонена');
+        else {
+            Yii::app()->user->setFlash('error', 'Ошибка при смене статуса');
+            $this->redirect(['index']);
+        }
+        if ($model->status == Invite::INVITE_CREATE) {
+            $mail = new YiiMailer();
+            $mail->setView('invite/decline');
+            $mail->setFrom('marklangovoi@gmail.com', 'Система управления учебным расписанием');
+            $mail->setTo($model->email);
+            $mail->setSubject('Заявка на создание');
+            $mail->send();
+            if (!$mail->send())
+                Yii::app()->user->setFlash('error', 'Ошибка при отправке письма');
+        }
         $this->redirect(['index']);
     }
 }
