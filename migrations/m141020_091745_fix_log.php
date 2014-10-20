dfd4d1388897805e973858ad5ab33588bd74e299
Mark Langovoi <marklangovoi@gmail.com>
2014-10-20 13:21:02 +0400

    Исправлены ошибки

--- migrations/m141020_091745_fix_log.php
+++ migrations/m141020_091745_fix_log.php
@@ -5,7 +5,7 @@ class m141020_091745_fix_log extends CDbMigration
     public function safeUp()
     {
 		/** @var ActiveRecordLog $log */
-		foreach (ActiveRecordLog::model()->findAllByAttributes([], 'description LIKE :text', [':text' => '%User Guest%']) as $log) {
+		foreach (ActiveRecordLog::model()->findAll('`description` LIKE :text', [':text' => '%Guest%']) as $log) {
 			$log->userid = null;
 			$log->save();
 		}
