b91edb3337df6f6541f38585b7fa626d58ab4b1c
Mark Langovoi <marklangovoi@gmail.com>
2014-10-21 19:21:18 +0400

    Добавлен выбор типа графика

--- controllers/IcsanalyticsController.php
+++ controllers/IcsanalyticsController.php
@@ -38,8 +38,10 @@ class IcsAnalyticsController extends Controller
         }
     }
 
-    public function actionChart()
+    public function actionChart($type = 'day')
     {
+        if (!in_array($type, ['day', 'hour']))
+            throw new CHttpException(404);
         $data = [];
         $temp_data = [];
         $criteria = new CDbCriteria();
@@ -47,12 +49,17 @@ class IcsAnalyticsController extends Controller
         $criteria->group = 'time';
         /** @var IcsAnalytics $ics_analytic_element */
         foreach (IcsAnalytics::model()->findAll($criteria) as $ics_analytic_element) {
-            $time = strtotime(explode(' ', $ics_analytic_element->time)[0]) * 1000;
+            $exploded = explode(' ', $ics_analytic_element->time);
+            $date = $exploded[0];
+            if ($type == 'hour') {
+                $time = explode(':', $exploded[1])[0];
+            } else $time = '00';
+            $time = strtotime($date . ' ' . $time . ':00:00') * 1000;
             if (isset($temp_data[$time]))
                 $temp_data[$time] += (int)$ics_analytic_element->count;
             else $temp_data[$time] = (int)$ics_analytic_element->count;
         }
-        foreach($temp_data as $date => $count)
+        foreach ($temp_data as $date => $count)
             $data[] = [$date, $count];
         $this->render('chart', ['series' => [['name' => 'Количество запросов:', 'data' => $data]]]);
     }
