fbaf3ccd7bf7b8b66c0fd18959b3767036fafc64
Mark Langovoi <marklangovoi@gmail.com>
2014-11-14 22:38:45 +0300

    Исправлена подгрузка заявок

--- controllers/GroupinviteController.php
+++ controllers/GroupinviteController.php
@@ -29,11 +29,6 @@ class GroupInviteController extends Controller
         if (!Yii::app()->request->isAjaxRequest && !Yii::app()->request->getParam('ajax'))
             $this->render('list', ['dataProvider' => $dataProvider, 'model' => $model]);
         else {
-            $model->setAttributes(Yii::app()->request->getParam('Invite'));
-            $dataProvider = $model->search();
-            $dataProvider->setSort([
-                'defaultOrder' => 'time DESC',
-            ]);
             $this->renderPartial('_list', ['dataProvider' => $dataProvider, 'model' => $model]);
         }
     }
